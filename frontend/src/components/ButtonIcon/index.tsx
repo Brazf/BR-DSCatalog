889bda5e7e3422cc66cdbb2318b30753af42f21d
Fábio Braz <fabiobrazferreira@gmail.com>
2023-04-16 23:26:13 -0300

    update

--- frontend/src/components/ButtonIcon/index.tsx
+++ frontend/src/components/ButtonIcon/index.tsx
@@ -1,14 +1,13 @@
+import './styles.css';
 import { ReactComponent as SetaIcon } from 'assets/images/Seta.svg';
 
 const ButtonIcon = () => {
   return (
     <div className="btn-container">
-      <div>
         <button className="btn btn-primary">
-          <h6>Inicie agora a sua busca</h6>
+          <h6>INICIE AGORA SUA BUSCA</h6>
         </button>
-      </div>
-      <div>
+      <div className="btn-icon-container">
         <SetaIcon />
       </div>
     </div>
